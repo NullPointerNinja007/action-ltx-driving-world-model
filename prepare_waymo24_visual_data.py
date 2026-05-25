5e243b51458d309f8edbf9f20a56a9c234cdcb57
NullPointerNinja007 <raddaigalamaleeka@gmail.com>
2026-05-24 18:50:48 -0700

    updates

---
+++ prepare_waymo24_visual_data.py
@@ -0,0 +1,3 @@
+"""Compatibility wrapper for the Waymo 24-FPS visual-continuation data pipeline."""
+
+from pipelines.data.prepare_waymo24_visual_data import *  # noqa: F401,F403
---
+++ train_ltx2b_waymo_visual_lora.py
@@ -0,0 +1,3 @@
+"""Compatibility wrapper for the LTX-2B Waymo visual LoRA training pipeline."""
+
+from pipelines.training.train_ltx2b_waymo_visual_lora import *  # noqa: F401,F403
